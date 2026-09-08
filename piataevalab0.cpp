112d8e388711c6b7e4cc7fc25989fd136d452044
pataevauliana51-jpg <pataevauliana51@gmail.com>
2026-09-08 12:40:48 -0700

    добавлена переменная x

--- piataevalab0.cpp
+++ piataevalab0.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 int main()
 { 
-    std::cout << "Hello, World!\n";
+    //std::cout << "Hello, World!\n";
+    int x;
+    std::cin >> x;
+    if (x !=0)
+        std::cout << 100/x;
     return 0;
 }
